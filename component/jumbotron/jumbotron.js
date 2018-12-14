705d5e17e602de95fa3b2bc30899feca06973dfa
joel jesus mota <joeljesusmot@gmail.com>
2018-12-14 17:53:15 -0400

    initial design

---
+++ component/jumbotron/jumbotron.js
@@ -0,0 +1,18 @@
+var title="CodeTeamRD";
+var p="Equipo de jovenes emprendedores en el mundo del desarrollo de software, trabajamos para todos los OS de dispositivos moviles, escritorio y web.<hr>Correo: <b>codeteamrd@outlook.com</b>";
+var p1="";
+
+document.write(`
+<div class="jumbotron">
+ <h4 class="display-4">${title}</h4>
+ <p class="lead">${p}</p>
+ <hr class="my-4">
+ <p>${p1}</p>
+ <p>Instagram: 
+   <a 
+    href="https://www.instagram.com/codeteamrd/?utm_source=ig_profile_share&igshid=rp4vzpgesk50" target="_blank">
+    <b> codeteamrd</b>
+   </a>
+ </p>
+</div>
+`);
---
+++ component/navbar/navbar.js
@@ -0,0 +1,19 @@
+document.write(`
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="../index.html"><img src="../img/panda1.png" width="30" height="30"> <b>Joel</b></a>
+
+
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="../index.html">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../pages/galery.html">Galeria</a>
+        </li>
+        <li class="nav-item">
+                <a class="nav-link" href="../pages/blog.html">Blog</a>
+        </li>
+        
+    </ul>
+</nav>
+`);
