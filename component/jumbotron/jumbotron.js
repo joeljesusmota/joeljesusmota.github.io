fa08a55ca9c9b9d5e1ff4bdded612f80f5aa97b5
joel jesus mota <joeljesusmot@gmail.com>
2018-12-19 20:02:05 -0400

    modified main.css

--- component/jumbotron/jumbotron.js
+++ component/jumbotron/jumbotron.js
@@ -1,19 +1,49 @@
-
-var title="CodeTeamRD";
-var p="Equipo de jovenes emprendedores en el mundo del desarrollo de software, trabajamos para todos los OS de dispositivos moviles, escritorio y web.<hr>Correo: <b>codeteamrd@outlook.com</b>";
-var p1="";
-
 document.write(`
-<div class="jumbotron">
- <h4 class="display-4">${title}</h4>
- <p class="lead">${p}</p>
- <hr class="my-4">
- <center><p>${p1}</p></center>
- <p>Instagram: 
-   <a 
-    href="https://www.instagram.com/codeteamrd/?utm_source=ig_profile_share&igshid=rp4vzpgesk50" target="_blank">
-    <b> codeteamrd</b>
-   </a>
- </p>
-</div>
+        <div class="container-fluid">
+        <div class="jumbotron">
+
+            <div class="row">
+                <div class="col-sm">
+                    <img src="../img/panda1.png" width="30" height="30"><b> Joel Mota</b>
+                    <p></p> 
+                    <p>© Copyright 2018 - 2019</p>
+                    <p></p> 
+                </div>
+                <div class="col-sm">
+                    <p><b>Menu</b></p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="..index.html">Home</a></li>
+                        <li class="list-group-item"><a href="../pages/aboutMe.html">About Me</a></li>
+                        <li class="list-group-item"><a href="../pages/blog.html">Blog</a></li>
+                        <li class="list-group-item"><a href="../pages/gallery.html">Gallery</a></li>
+
+                    </ul>
+
+                </div>
+                <div class="col-sm">
+                    <center>
+                        <b>Contact</b><br>
+                        
+                            <address class="p-g">Mail: joeljesusmot@gmail.com</address>
+                            <address class="p-g">Mobile: 829-522-0445</address>
+                            <address class="p-g">Address: Dom.Rep, Santo Domingo, National District</address>
+
+                    </center>
+
+                    <hr />
+                    
+                    <center>
+                        
+                    <a href="https://www.instagram.com/joelmota04/" target="_blank"><img src="/img/il.png" style="height: 40px; width: 40px;"></a>
+                    <a href="https://twitter.com/joeljmota" target="_blank"><img src="/img/tl.png" style="height: 40px; width: 40px;"></a>
+                    <a href="https://do.linkedin.com/in/joel-jes%C3%BAs-mota-79b058156" target="_blank"><img src="/img/ll.png" style="height: 40px; width: 40px;"></a>
+                    <a href="https://www.facebook.com/profile.php?id=100009707984923" target="_blank"><img src="/img/fl.png" style="height: 40px; width: 40px;"></a>
+                     
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+
 `);
+
