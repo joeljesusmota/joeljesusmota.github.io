6308a2d14ea90b29f172f96cc2f42a3d91f62a71
joel jesus mota <joeljesusmot@gmail.com>
2019-11-12 12:36:55 -0400

    update

--- component/navbar/navbar.js
+++ component/navbar/navbar.js
@@ -1,8 +1,53 @@
-var URLactual = window.location;
+/*function NavBarComponent(){
+    var active;
+    var URLactual = window.location;
 
-if(URLactual=="file:///C:/JJ/joeljesusmota.github.io/pages/blog.html")
-{
-    document.write("estoy en blog");
-}else{
-    document.write("home");
-}
+    if(URLactual){
+
+    }else{
+        
+    }
+    
+    switch(!URLactual) {
+        case "https://joeljesusmota.github.io/pages/about.html":
+            active = "active";
+          break;
+        case "":
+          // code block
+          break;
+        default:
+          // code block
+      }
+
+return document.write(`
+<!--navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.html">
+        <img src="img/panda1.png" width="30" height="30">
+        <b>Joel</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-h3="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link ${}" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${}" href="pages/about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${}" href="pages/blog.html">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${}" href="pages/gallery.html">Gallery</a>
+            </li>
+    </div>
+</nav>
+<!--/navbar-->
+`);
+
+};
+*/
